9145508e6c6dadb16985806265d76adafd0d37f3
leonardzz7 <leonard@leooli.dk>
2023-11-06 15:09:55 +0100

    Foundation

--- ZooObjektorienteretProgram/Button.cs
+++ ZooObjektorienteretProgram/Button.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace ZooObjektorienteretProgram
 {
     internal class Button
     {
+        public Rectangle rect;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+
+
+
+
+
+
     }
 }
--- ZooObjektorienteretProgram/Player.cs
+++ ZooObjektorienteretProgram/Player.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace ZooObjektorienteretProgram
 {
     internal class Player
     {
+        private Point mousePosition;
+        private MouseState mouseState;
+        private bool pressed;
+
+
+        public Player()
+        {
+
+        }
+
+        public void MouseUpdate()
+        {
+            Rectangle tempRect = new Rectangle();
+
+
+            mousePosition = new Point(mouseState.X, mouseState.Y);
+            mouseState = new MouseState();
+
+
+            // Check if the mouse position is inside the rectangle
+            if (tempRect.Contains(mousePosition) && mouseState.LeftButton == ButtonState.Pressed && pressed == false)
+            {
+                pressed = true;
+                
+            }
+            else if (mouseState.LeftButton == ButtonState.Released)
+            {
+                pressed = false;
+            }
+            {
+                
+            }
+            
+        }
+
     }
 }
