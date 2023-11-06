1a8947492983facf37dc693a1f5061a7cd556b51
F0gboy <Jasp3r@live.dk>
2023-11-06 20:30:48 +0100

    STUFF AGAIN

--- ZooObjektorienteretProgram/AnimalBoundaries.cs
+++ ZooObjektorienteretProgram/AnimalBoundaries.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Reflection.Metadata;
 using Microsoft.Xna.Framework.Content;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace ZooObjektorienteretProgram
 {
@@ -18,23 +19,29 @@ namespace ZooObjektorienteretProgram
         private List<Texture2D> boundarySprites = new List<Texture2D>(6);
         private int boundarySize;
 
-        public AnimalBoundaries(int BoundarySize, List<Texture2D> BoundarySprites) 
+        public AnimalBoundaries(int BoundarySize, List<Texture2D> BoundarySprites, SpriteBatch spriteBatch) 
         { 
             this.boundarySize = BoundarySize;
             this.boundarySprites = BoundarySprites;
+            this._spriteBatch = spriteBatch;
         }
 
         private void LoadContent(ContentManager content)
         {
             for (int i = 0; i < boundarySprites.Count; i++)
             {
-                boundarySprites[i] = content.Load<Texture2D>(i + 1 + "Fence");
+                this.boundarySprites[i] = content.Load<Texture2D>(i + 1 + "Fence");
             }
         }
 
         private void DrawBoundary(GameTime gameTime)
         {
-            
+            for(int i = 0;i < boundarySprites.Count;i++) 
+            {
+                _spriteBatch.Draw(boundarySprites[i], position, null, Color.White, 0, origin, 1, SpriteEffects.None, 0);
+
+            }
+
         }
     }
 }
