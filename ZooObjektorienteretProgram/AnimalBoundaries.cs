13c42ea6094d1b588c5d1a53c05e81e897e2ffc6
F0gboy <Jasp3r@live.dk>
2023-11-06 13:36:38 +0100

    stads

--- ZooObjektorienteretProgram/AnimalBoundaries.cs
+++ ZooObjektorienteretProgram/AnimalBoundaries.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,16 @@ namespace ZooObjektorienteretProgram
 {
     internal class AnimalBoundaries
     {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Rectangle rectangle;
+        private List<Texture2D> boundarySprites;
+        private int boundarySize;
+
+        public AnimalBoundaries(int BoundarySize, List<Texture2D> BoundarySprites) 
+        { 
+            boundarySize = BoundarySize;
+            this.boundarySprites = BoundarySprites;
+        }
     }
 }
