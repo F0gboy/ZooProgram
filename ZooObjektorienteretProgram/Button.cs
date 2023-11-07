b2c78c5e08cddc01a0190a069cb2c3d9823994c3
leonardzz7 <leonard@leooli.dk>
2023-11-07 10:33:35 +0100

    Button Alts

--- ZooObjektorienteretProgram/Button.cs
+++ ZooObjektorienteretProgram/Button.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+//using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -13,17 +15,25 @@ namespace ZooObjektorienteretProgram
     {
         public Rectangle rect;
         private GraphicsDeviceManager graphics;
+        private Texture2D sprite;
         private SpriteBatch spriteBatch;
 
-        public override void Draw(GameTime GameTime)
+        public Button()
         {
 
+        }
+        public override void Draw(GameTime GameTime)
+        {
+            spriteBatch.Begin(samplerState: SamplerState.PointWrap);
+            spriteBatch.Draw(sprite, rect, Color.White);
+            spriteBatch.End();
 
         }
 
         public override void LoadContent()
         {
-
+            
+            sprite = content.Load<Texture2D>("Button");
 
         }
     }
